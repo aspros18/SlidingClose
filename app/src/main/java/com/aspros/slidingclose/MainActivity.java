06df38ad531499556bd4569b3faf9e529f783d48
aspros18 <aimar18@126.com>
2018-04-12 01:11:19 +0800

    添加滑动关闭测试Activity

--- app/src/main/java/com/aspros/slidingclose/MainActivity.java
+++ app/src/main/java/com/aspros/slidingclose/MainActivity.java
@@ -1,13 +1,30 @@
 package com.aspros.slidingclose;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends SlideActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ViewPagerActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected boolean enableSliding() {
+        // 关闭左侧滑动
+        return false;
     }
 }
---
+++ app/src/main/java/com/aspros/slidingclose/NormalActivity.java
@@ -0,0 +1,11 @@
+package com.aspros.slidingclose;
+
+import android.os.Bundle;
+
+public class NormalActivity extends SlideActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal);
+    }
+}
